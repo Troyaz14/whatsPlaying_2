3ebb0653b3efcbe9214b64b885645e326c287e74
Troy Gugler <gugler@gmail.com>
2016-01-09 15:01:22 -0700

    Version 1, all emails added and working

--- NewEmailAppMom/app/src/main/java/com/example/android/newemailappmom/MainActivity.java
+++ NewEmailAppMom/app/src/main/java/com/example/android/newemailappmom/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.android.newemailappmom;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
-
+    String eMail=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,5 +41,52 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void exitApp(View myView){
+        this.finish();
+        System.exit(0);
+    }
+    public void email(View myView){
+        eMail="";
+        //View view =  getLayoutInflater().inflate(R.layout.content_main, null);
+
+        CheckBox checkBox1 = (CheckBox) findViewById(R.id.contact1);
+        CheckBox checkBox2 = (CheckBox) findViewById(R.id.contact2);
+        CheckBox checkBox3 = (CheckBox) findViewById(R.id.contact3);
+        CheckBox checkBox4 = (CheckBox) findViewById(R.id.contact4);
+        CheckBox checkBox5 = (CheckBox) findViewById(R.id.contact5);
+        CheckBox checkBox6 = (CheckBox) findViewById(R.id.contact6);
+        CheckBox checkBox7 = (CheckBox) findViewById(R.id.contact7);
+
+        if(checkBox1.isChecked()==true){
+            eMail = "guglert80@gmail.com; ";
+        }
+
+        System.out.println("checkbox 1 checked? " + checkBox1.isChecked());
+        if(checkBox2.isChecked()==true){
+            eMail = eMail+"cory.gugler@gmail.com; ";
+        }
+        if(checkBox3.isChecked()==true){
+            eMail = eMail+"mgugler@gmail.com; ";
+        }
+        if(checkBox4.isChecked()==true){
+            eMail = eMail+"pgugler@gmail.com; ";
+        }
+        if(checkBox5.isChecked()==true){
+            eMail = eMail+"adrian.gugler@gmail.com; ";
+        }
+        if(checkBox6.isChecked()==true){
+            eMail = eMail+"brengugler@gmail.com; ";
+        }
+        if(checkBox7.isChecked()==true){
+            eMail = eMail+"herbg40@gmail.com; ";
+        }
+
+        Intent i = new Intent(Intent.ACTION_SENDTO,
+                Uri.fromParts("mailto", eMail, null)
+        );
+        i.putExtra(Intent.EXTRA_SUBJECT, "Hi from Mom.");
+        startActivity(i);
     }
 }
